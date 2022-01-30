e3a88b1768db0554bff7d5947fb808ef29dd3eab
ailict <ailichatfieldtaylor@gmail.com>
2022-01-30 11:07:09 -0500

    license badge and link functions

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -20,11 +20,32 @@ function renderLicenseBadge(data) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(data) {
+  if (data == 'MIT') {
+    return `${renderLicenseBadge}(https://opensource.org/licenses/MIT)`;
+
+  } else if (data == 'APACHE 2.0') {
+    return `${renderLicenseBadge}(https://opensource.org/licenses/Apache-2.0)`;
+
+  } else if (data == 'GPL 3.0') {
+    return `${renderLicenseBadge}(https://www.gnu.org/licenses/gpl-3.0)`;
+
+  } else if (data == 'BSD 3') {
+    return `${renderLicenseBadge}(https://opensource.org/licenses/BSD-3-Clause)`;
+
+  } else if (data == 'None') {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(data) {
+  renderLicenseLink();
+  if (data === '') {
+    return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
