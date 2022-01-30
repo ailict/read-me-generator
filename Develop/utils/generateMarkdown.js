32caa62cf4971e2cb8be884f209a0c09f1501f7a
ailict <ailichatfieldtaylor@gmail.com>
2022-01-30 11:00:50 -0500

    render license function

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,22 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(data) {
+  if (data == 'MIT') {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`;
+
+  } else if (data == 'APACHE 2.0') {
+    return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]`;
+
+  } else if (data == 'GPL 3.0') {
+    return `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)]`;
+
+  } else if (data == 'BSD 3') {
+    return `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)]`;
+
+  } else if (data == 'None') {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
